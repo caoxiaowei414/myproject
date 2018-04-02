bccf52ae3d1b1c31c4bc87a5d337350f452b6dbd
caoxiaowei <337619617@qq.com>
2018-04-02 13:21:52 +0800

    meiyou ch

--- src/main/java/MygitHub.java
+++ src/main/java/MygitHub.java
@@ -4,5 +4,9 @@ public class MygitHub {
     public static void main(String[] args) {
         System.out.println();
        String ss  = "shangchauchengg ";
+
+        ss  = "shangchauchengg ";
+       ss  = "shangchauchengg ";
+
     }
 }
